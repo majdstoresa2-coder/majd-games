f9ac5e79b0743a0aaa992ba1b6b21f0ecb2f2585
majdstoresa2-coder <majd.store.sa2@gmail.com>
2026-08-07 02:27:10 +0300

    Create MajdAIControlCenter.jsx

---
+++ src/components/MajdAIControlCenter.jsx
@@ -0,0 +1,14 @@
+import AIStatusCard from "./AIStatusCard";
+
+export default function MajdAIControlCenter(){
+
+  return (
+    <div>
+      <AIStatusCard />
+
+      <button>
+        تشغيل الذكاء الاصطناعي
+      </button>
+    </div>
+  );
+}
